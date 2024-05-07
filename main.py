5cdc1866f350a3721a09dc481981328f69d381b7
ADISH-04 <720721115004@hicet.ac.in>
2024-05-07 14:19:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+def is_palindrome(x):
+    if x<0:
+        return False
+    else:
+        x=str(x)
+        return x==x[::-1]
+print(is_palindrome(121))
+print(is_palindrome(-121))
+print(is_palindrome(10))
